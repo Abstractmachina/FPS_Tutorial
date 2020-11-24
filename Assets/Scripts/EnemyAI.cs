a99633fa30523c717d7de89f56a862cc3d63c0f6
AM_00 <abstractmachina@gmail.com>
2020-11-24 23:52:31 +0000

    175 Give That Player A Gun: basic sandbox level, enemy behavior and gun in FPS view.

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] public Transform target;
+    [SerializeField] public float chaseRange = 5f;
+
+    private NavMeshAgent navMeshAgent;
+    private float distanceToTarget = Mathf.Infinity;
+    private bool isProvoked = false;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+    }
+
+    void Update()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if (isProvoked)
+        {
+            EngageTarget();
+        }
+        else if (distanceToTarget < chaseRange)
+        {
+            isProvoked = true;
+            ChaseTarget();
+        }
+    }
+
+    private void EngageTarget()
+    {
+
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        if (distanceToTarget < navMeshAgent.stoppingDistance)
+        {
+            Debug.Log("Attacking!");
+        }
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+}
