5d5508370b736e4da9f03626ebd77a0e3300df6e
AM_00 <abstractmachina@gmail.com>
2021-03-04 09:10:41 +0000

    160 Enemy Health & Damage

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+
+    [SerializeField] float hitpoints = 100f;
+
+    //create public mtehod reducing hitpoints by amount of dmg.
+    public void TakeDamage(float damage)
+    {
+        hitpoints -= damage;
+        Debug.Log("enemy health: " + hitpoints);
+
+        if (hitpoints <= 0)
+        {
+            Debug.Log("enemy died");
+            Destroy(this.gameObject);
+        }
+
+    }
+}
